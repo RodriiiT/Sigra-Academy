718c5f5721d4b328c8377bfa85777f1bbe2928c8
JoSantana0005 <joseasantana05@gmail.com>
2026-01-15 21:18:36 -0400

    feat: :hammer: Se agrega la parte de la incripción de un estudiante a una sección

--- backend/src/api/routes/api.routes.mjs
+++ backend/src/api/routes/api.routes.mjs
@@ -3,6 +3,7 @@ import { SETTINGS } from "../../../config/settings.config.mjs";
 import { AssignmentRouter } from "../../modules/academic-manager-III/assignments/assignment.route.mjs";
 import { managerRoutes } from "../../modules/academic-manager-III/manager.route.mjs";
 import { SchedulesRoutes } from "../../modules/academic-manager-III/schedules/schedules.route.mjs";
+import { EnrollmentRouter } from "../../modules/academic-structure-II/enrollments/enrollment.route.mjs";
 import { GradeRoutes } from "../../modules/academic-structure-II/grades/grade.route.mjs";
 import { prelaciesRoute } from "../../modules/academic-structure-II/prelacies/prelacies.route.mjs";
 import { SectionRoutes } from "../../modules/academic-structure-II/sections/section.route.mjs";
@@ -48,7 +49,8 @@ export const ListRoutes = {
         prelacies: router.use(`${SETTINGS.BASE_PATH}/prelacies`, prelaciesRoute),
         sections: router.use(`${SETTINGS.BASE_PATH}/sections`, SectionRoutes),
         gradeAcademic: router.use(`${SETTINGS.BASE_PATH}/grades`, GradeRoutes),
-        years: router.use(`${SETTINGS.BASE_PATH}/years`, YearRoutes)
+        years: router.use(`${SETTINGS.BASE_PATH}/years`, YearRoutes),
+        enrollments: router.use(`${SETTINGS.BASE_PATH}/enrollments`, EnrollmentRouter)
     },
     grades: {
         grades: router.use(`${SETTINGS.BASE_PATH}/grades-log`, GradesLogRoutes),
